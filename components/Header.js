ecb64a67182ccc80b41e6213f15b545b4ef1e9cb
Pedro Carvalho <80.nicolau@gmail.com>
2022-09-24 17:50:04 +0100

    remove container class

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import Menu from "./Menu";
 export default function Header(props) {
   return (
     <div {...props}>
-      <div className="container sm:mx-10 md:mx-10 lg:mx-52 py-4">
+      <div className="sm:mx-10 md:mx-10 lg:mx-52 py-4">
         <Menu />
       </div>
     </div>
